b19451099ef1d6b08524803bf10654746bd3f1b0
Mingliang Wang <wang_m@worksap.co.jp>
2019-10-24 01:10:14 +0800

    texton: support text range

--- src/texton.ts
+++ src/texton.ts
@@ -23,4 +23,18 @@ export namespace ITextRange {
     from: {text: '', nth: -1},
     to: {text: '', nth: -1},
   };
+
+  export function toSimpleString(range: ITextRange): string {
+    return JSON.stringify([`${range.from.nth}:${range.from.text}`, `${range.to.nth}:${range.to.text}`]);
+  }
+
+  export function fromSimpleString(str: string): ITextRange {
+    const simple: [string, string] = JSON.parse(str);
+    const from = simple[0].split(':', 2);
+    const to = simple[1].split(':');
+    return {
+      from: {text: from[1], nth: parseInt(from[0], 10)},
+      to: {text: to[1], nth: parseInt(to[0], 10)}
+    }
+  }
 }
